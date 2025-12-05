6db0e0172c8fa278f355ad92aa84cad003506832
bcgwebdesign <github@bcgwsebdesign.co.uk>
2025-12-05 11:36:29 +0000

    WIP on new parsed handbook

---
+++ content/handbook.md
@@ -0,0 +1,13 @@
++++
+title = "handybook"
+date = "2019-03-26T08:47:11+01:00"
+draft = false
+layout = "handbooky"
+# the following section is for the header
+imagetop = "The Identifier"
+imagemain = "DOI Handbook"
+maintext = "The DOI Handbook includes all the technical information that DOI agencies and users need to know"
+subtext = "A new version was published in 2025."
++++
+
+Some content here
--- themes/doi-theme/static/js/script.js
+++ themes/doi-theme/static/js/script.js
@@ -73,6 +73,30 @@ $(function() {
   }
 });
 
+(function($) {
+  $('nav a').on('click', function() {
+    $('.triangle-container').remove();
+      show_content($(this).index());
+  });
+  
+  show_content(0);
+
+  function show_content(index) {
+    // Make the content visible
+    $('.tabs .content.visible').removeClass('visible');
+    $('.tabs .content:nth-of-type(' + (index + 1) + ')').addClass('visible');
+
+    // Set the tab to selected
+    $('nav a.selected').removeClass('selected');
+    $('nav a:nth-of-type(' + (index + 1) + ')').addClass('selected');
+    
+    // How to remove the arrow and only show it on the selected tab?
+  }
+})( jQuery ); 
+
+
+
+
 
 
 
